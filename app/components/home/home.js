6d3fbc302ccb5efe7626497785112e1a0e68434e
giandiaz <giandiaz@ucm.es>
2016-10-02 21:43:29 +0200

    changes design

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -2,10 +2,11 @@
 angular.module('app')
 	.controller('homeCtrl', ['mockService',function (mockService) {
 		var vm = this;
-		vm.slides = mockService.images();
+		vm.slides = mockService.series();
+		vm.homeComment = mockService.comments('home');
+		console.log(vm.homeComment)
 		vm.active = 0;
 		vm.noWrapSlides = false;
 		vm.myInterval = 4000;
-		console.log(vm.slides)
 
 	}]);
--- app/shared/services/mockService.js
+++ app/shared/services/mockService.js
@@ -2,10 +2,21 @@
 angular.module('app')
 	.factory('mockService', ['appConstants' , function (appConstants) {
 		var service = {};
+		var comments = {
+			home: {
+				title:"Bienvenidos",
+				text:"Un pequeño espacio libre de publicidad para los amantes de las series de ficción." +
+				" Aqui encontrareís los tíulos mas famosos del genero y un fácil acceso a sus capítulos." +
+				"Ponte cómodo y a disfrutar"
+			}
+		};
 		service.series = function (){
-			var series=[];
-			return series;
-		}
+			return [];
+		};
+
+		service.comments = function (bind) {
+			return comments[bind];
+		};
 
 
 		return service;
